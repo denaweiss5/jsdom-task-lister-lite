5163f530afccdcdefd971847f1139c6225841b94
https://learn.co/DenaNeuman <denaweiss5@gmail.com>
2020-12-10 19:32:07 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskForm = document.querySelector('#create-task-form');
+  const taskList = document.querySelector('#tasks'); 
+
+  taskForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const newTaskInput = document.getElementById("new-task-description").value;
+
+    taskList.innerHTML += `<li> ${newTask}
+    <button data-action = "delete"> X </button>
+    </li>`;
+    taskForm.reset();
+
+});
+
+taskList.addEventListener("click", function(e) {
+  if (e.target.dataset.action === "delete") {
+    e.target.parentElement.remove()
+  }
 });
